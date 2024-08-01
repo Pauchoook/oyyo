01c943cc017f9196a808cc4126b0bfb0097f297a
Pauchoook <nkolatarasov@mail.ru>
2024-08-01 20:15:26 +0300

    started modal

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,9 @@ import * as functions from "./files/functions.js";
 import "../scss/style.scss";
 import spoller from "./files/spoller.js";
 import burger from "./files/burger.js";
+import modal from "./files/modal.js";
 
 functions.isWebp();
 spoller();
 burger();
+modal();
